d87a91c4889c38a8fa44d6377e88c72386d01363
Sadikul Islam <sh.sifatallrafi.08@gmail.com>
2022-11-10 21:20:25 +0600

    private route setup

--- src/Pages/MyReviews/MyReviews.js
+++ src/Pages/MyReviews/MyReviews.js
@@ -1,10 +1,12 @@
 import React, {useContext,useEffect,  useState} from 'react';
 import { AuthContext } from '../../context/AuthProvider/AuthProvide';
 import Review from './Review';
+import useTitle from './../../hooks/useTitle';
 
 const MyReviews = () => {
     const {user} = useContext(AuthContext);
     const [reviews , setReviews] = useState([]);
+    useTitle("Review")
 
 
     useEffect(() => {
@@ -16,6 +18,7 @@ const MyReviews = () => {
     }, [user?.email])
 
     const handleDelete = id => {
+        console.log(id)
 
         const proceed = window.confirm("Are you sure you want to delete this review ?")
         if(proceed){
@@ -27,13 +30,15 @@ const MyReviews = () => {
                 console.log(data);
                 if(data.deletedCount > 0){
                     alert('Deleted succesfully')
+                    const remaing = reviews.filter(review => review._id !== id);
+                    setReviews(remaing)
                 }
             })
         }
     }
 
     return (
-        <div className='p-5 md:p-9 grid grid-cols-1 md:grid-cols-2 gap-10'>
+        <div className='p-5 min-h-screen md:p-9 grid grid-cols-1 md:grid-cols-2 gap-10'>
           {
             reviews.map(review => <Review
             key={reviews._id}
