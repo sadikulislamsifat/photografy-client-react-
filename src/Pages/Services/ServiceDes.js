97c36d13172f195d3f0bcfde7747dc3740d76917
Sadikul Islam <sh.sifatallrafi.08@gmail.com>
2022-11-10 17:15:24 +0600

    about page added

--- src/Pages/Services/ServiceDes.js
+++ src/Pages/Services/ServiceDes.js
@@ -14,13 +14,14 @@ const ServiceDes = () => {
     return (
         <div>
            <div className="card p-3 lg:card-side bg-base-100 shadow-xl w-full my-9">
-            <div className='md:w-1/2'>
+            <div className='md:w-1/2 h-full'>
                 
-  <figure><img src={banner} className="md:w-1/" alt=""/></figure>
+  <figure><img src={banner} className="w-full h-full" alt=""/></figure>
             </div>
   <div className="card-body md:w-1/2">
-    <h2 className="card-title">{service_name}</h2>
+    <h2 className="card-title md:text-4xl">{service_name}</h2>
     <p>{description}</p>
+    <p className='font-semibold text-red-500'>Per Picture Price ${rent}</p>
     <div className="card-actions justify-end">
       {/* <button className="btn btn-primary">Listen</button> */}
     </div>
